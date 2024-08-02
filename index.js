11c71ea6046bad1d02c0c37387f5fa2bf44f7d02
Nishant <ntih5565@gmail.com>
2024-08-02 20:39:27 +0530

    first push

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const ytdl = require('ytdl-core')
+
+const app = express()
+var cors = require("cors");
+
+app.use(cors({
+    origin: 'http://localhost:8080'
+}))
+
+app.get('/download', async (req, res) => {
+    try {
+        const url = req.query.url
+        const videoId = ytdl.getURLVideoID(url)
+        const metaInfo = await ytdl.getInfo(url)
+        let data = {
+            url: 'https://www.youtube.com/embed/'+videoId,
+            info: metaInfo.formats
+        }
+        return res.send(data)
+    } catch(error) {
+        return res.status(500)
+    }
+})
+
+app.listen(4000, () => {
+    console.log(`Server is running on PORT: 4000`)
+})
