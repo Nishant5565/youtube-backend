10ff022970a1613d0288b1ef7fb5ef52f0abd056
Nishant <ntih5565@gmail.com>
2024-08-09 16:36:09 +0530

    major changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors(
     {
-        origin: ['http://localhost:3000', 'https://youtubenks.netlify.app']
+        origin: ['http://localhost:5173', 'https://youtubenks.netlify.app']
     }
 ));
 
