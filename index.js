c224dd59451930481a9573c850604b5ffa02d920
Nishant Kumar Singh <ntih5565@gmail.com>
2024-08-09 15:44:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,7 @@ const ytdl = require('ytdl-core')
 const app = express()
 var cors = require("cors");
 
-const allowedOrigins = [
-  'https://youtubenks.netlify.app',
-    'http://localhost:5173/'
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
-app.use(cors(corsOptions));
+app.use(cors({ origin: 'http://localhost:5173/' }))
 
 app.get('/download', async (req, res) => {
     try {
