5d20be524afd04a866f6dd875aab1a9dd98191bc
Nishant <ntih5565@gmail.com>
2024-08-09 16:34:18 +0530

    major changes

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-const express = require('express')
-const ytdl = require('ytdl-core')
+const express = require('express');
+const ytdlCore = require('ytdl-core');
+const cors = require('cors');
 
-const app = express()
-var cors = require("cors");
-
-app.use(cors({ origin: 'http://localhost:5173/' }))
-
-app.get('/download', async (req, res) => {
-    try {
-        const url = req.query.url
-        const videoId = ytdl.getURLVideoID(url)
-        const metaInfo = await ytdl.getInfo(url)
-        let data = {
-            url: 'https://www.youtube.com/embed/'+videoId,
-            info: metaInfo.formats
-        }
-        return res.send(data)
-    } catch(error) {
-        return res.status(500)
+const app = express();
+app.use(cors(
+    {
+        origin: ['http://localhost:3000', 'https://youtubenks.netlify.app']
     }
-})
+));
 
-app.listen(4000, () => {
-    console.log(`Server is running on PORT: 4000`)
-})
+app.use(express.json()); // Parse JSON bodies
+
+app.post('/download', async (req, res) => {
+    try {
+        const { data } = req.body;
+        console.log('Received URL:', data);
+
+        const videoId = ytdlCore.getURLVideoID(data);
+        const metaInfo = await ytdlCore.getInfo(data);
+        const audioFormats = ytdlCore.filterFormats(metaInfo.formats, 'audioonly');
+
+        const responseData = {
+            url: `https://www.youtube.com/embed/${videoId}`,
+            info: metaInfo.formats,
+            audioFormats
+        };
+
+        res.status(200).json(responseData);
+    } catch(error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Failed to process request' });
+    }
+});
+
+
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+});
